ec839959d148329cf7d6d861e2f9b7c937f97428
Peter Motyka <peter@motyka.org>
2015-03-18 20:06:00 -0700

    clean up comments

--- IntegerToBinaryString/IntegerToBinaryString.java
+++ IntegerToBinaryString/IntegerToBinaryString.java
@@ -1,18 +1,21 @@
+/*
+Example compile/run:
+$ javac IntegerToBinaryString.java
+$ java IntegerToBinaryString 2147483646
+1111111111111111111111111111110
+*/
 class IntegerToBinaryString
 {
     public static void main(String[] args) {
-       
-	//assume valid integer passed in as first command line arg,
-	//will throw a NFE is otherwise
+	//assume valid integer passed in as first command line arg, will throw an NFE if otherwise
         int i = Integer.parseInt(args[0]);
         StringBuffer buf = new StringBuffer();
         do {
 	    //insert bit in leftmost position
             buf.insert(0, i & 1);
-            //discard rightmost bit via bit shift / assignment
+            //discard rightmost bit via shift / assignment
             i >>= 1;
         } while (i != 0); //terminate when all bits discarded
-
         System.out.println(buf.toString());
     }
 }
